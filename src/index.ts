b57ee8d4c5a958f35f488b8c799ad87e68175f67
Admiro André <admiroalfredo1742@gmail.com>
2024-04-13 22:22:59 +0100

    Segundo commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-const soma = (x: number, y: number): number => x + y;
+import { Prisma, PrismaClient } from '@prisma/client';
+import express, { Request, Response } from 'express'
 
 
-console.log(soma(10, 0.5))
+const Application = express()
+
+const prisma = new PrismaClient()
+
+Application.get('/', (req: Request, res: Response) => {
+	return res.json({ api: 'sag'}).status(200)
+});
+
+Application.listen(3000, () => console.log("Running api..."))
